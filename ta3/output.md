f30ccc94b94ba077f7371d4578d025c4d0f875a7
jepaki <jepaki@mtu.edu>
2023-09-21 15:06:55 -0400

    Adding model and raster outputs to schema

--- ta3/output.md
+++ ta3/output.md
@@ -6,4 +6,74 @@
   - **Items** *(number)*
 - **`crs`** *(string)*: Coordinate reference system of the tile scheme.
 - **`resolution`** *(number)*: Resolution of the output prospectivity raster.
+- **`pipelines`** *(array)*
+  - **Items**: Refer to *[ModelRun](#ModelRun)*.
+- **`prospectivity_score`**: Prospectivity score raster.
+  - **All of**
+    - : Refer to *[ProspectivityScore](#ProspectivityScore)*.
+- **`prospectivity_uncertainty`**: Prospectivity uncertainty raster.
+  - **All of**
+    - : Refer to *[ProspectivityUncertainty](#ProspectivityUncertainty)*.
+
+## ConfidenceEstimation
+
+*Confidence information for a map extraction*
+
+### Properties
+
+- **`model`**: Refer to *[ExtractionIdentifier](#ExtractionIdentifier)*.
+- **`scale`**: Refer to *[ConfidenceScale](#ConfidenceScale)*.
+- **`confidence`** *(number)*: Certainty.
+- **`extra_data`** *(object)*: Additional data.
+
+## ConfidenceScale
+
+*Confidence measure for a map extraction*
+
+### Properties
+
+- **`name`** *(string)*: Name of the confidence scale.
+- **`description`** *(string)*: Description of the confidence scale.
+- **`min_value`** *(number)*: Minimum value.
+- **`max_value`** *(number)*: Maximum value.
+
+## ExtractionIdentifier
+
+*Link to extracted model*
+
+### Properties
+
+- **`model`** *(string)*: Model name.
+- **`id`** *(integer)*: ID of the extracted feature.
+- **`field`** *(string)*: Field name of the model.
+
+## ModelRun
+
+*Information about a model run.*
+
+### Properties
+
+- **`pipeline_name`** *(string)*: Model name.
+- **`version`** *(string)*: Model version.
+- **`timestamp`** *(string)*: Time of model run.
+- **`batch_id`**: Batch ID.
+  - **Any of**
+    - *string*
+    - *null*
+- **`image_size`** *(array)*: Pixel size of the map image.
+  - **Items** *(integer)*
+- **`confidence`** *(array)*
+  - **Items**: Refer to *[ConfidenceEstimation](#ConfidenceEstimation)*.
+
+## ProspectivityScore
+
+### Properties
+
+- **`uri`** *(string)*: URI of the output prospectivity score raster.
+
+## ProspectivityUncertainty
+
+### Properties
+
+- **`uri`** *(string)*: URI of the output prospectivity uncertainty raster.
 
--- ta3/output.py
+++ ta3/output.py
@@ -10,6 +10,58 @@ Point = TypeVar("Point")
 WKT = TypeVar("WKT", bound=str)
 
 
+class ExtractionIdentifier(BaseModel):
+    """Link to extracted model"""
+
+    model: str = Field(
+        ...,
+        description="Model name",
+        example=[
+            "PolygonFeature",
+            "LineFeature",
+            "PointFeature",
+            "MapUnit",
+            "LineType",
+            "PointType",
+        ],
+    )
+    id: int = Field(..., description="ID of the extracted feature")
+    field: str = Field(..., description="Field name of the model")
+
+
+class ConfidenceScale(BaseModel):
+    """Confidence measure for a map extraction"""
+
+    name: str = Field(..., description="Name of the confidence scale")
+    description: str = Field(..., description="Description of the confidence scale")
+    min_value: float = Field(..., description="Minimum value")
+    max_value: float = Field(..., description="Maximum value")
+
+
+class ConfidenceEstimation(BaseModel):
+    """Confidence information for a map extraction"""
+
+    model: ExtractionIdentifier
+    scale: ConfidenceScale
+    confidence: float = Field(..., description="Certainty")
+    extra_data: dict = Field(..., description="Additional data")
+
+class ModelRun(BaseModel):
+    """Information about a model run."""
+
+    pipeline_name: str = Field(..., description="Model name")
+    version: str = Field(..., description="Model version")
+    timestamp: str = Field(..., description="Time of model run")
+    batch_id: Optional[str] = Field(..., description="Batch ID")
+    image_size: list[int] = Field(..., description="Pixel size of the map image")
+    confidence: list[ConfidenceEstimation]
+
+class ProspectivityScore(BaseModel):
+    uri: str = Field(..., description="URI of the output prospectivity score raster")
+
+class ProspectivityUncertainty(BaseModel):
+    uri: str = Field(..., description="URI of the output prospectivity uncertainty raster")
+
 class ProspectivityModel(BaseModel):
     bounds: list[float] = Field(..., description="Bounding box of the tile scheme")
     crs: WKT = Field(
@@ -18,3 +70,8 @@ class ProspectivityModel(BaseModel):
         # default="EPSG:3857 (Web Mercator)",
     )
     resolution: float = Field(..., description="Resolution of the output prospectivity raster")
+
+    pipelines: list[ModelRun]
+    prospectivity_score: ProspectivityScore = Field(..., description="Prospectivity score raster")
+    prospectivity_uncertainty: ProspectivityUncertainty = Field(..., description="Prospectivity uncertainty raster")
+    feature_importance: list[dict] = Field(..., description="Feature importance")
