12c0243aa05daf6769d4f5c435a1c75a9fb70c84
YOO-uN-ee <jiyoonp0228@gmail.com>
2023-12-12 16:19:35 -0600

    Changes to ta2 mineral site reconciliation output schema

--- ta2/output.py
+++ ta2/output.py
@@ -135,8 +135,10 @@ class MineralSite(BaseModel):
     location_info: LocationInfo
     geology_info: GeologyInfo
     deposit_type: Optional[DepositType] = Field(description="The deposit type of an inventory item")
+    source_id: str = Field(description="Source dataset that the site info is retrieved from. e.g., MRDS")
+    record_id: str = Field(description="Unique ID of the record that the info is retrieved from e.g., 10022920")
     same_as: Optional[dict] = Field(
-        description='Dictionary that stores the IDs point to other databases: e.g.: {"MRDS" : [{"id":1,"Attributes": {"dep_id" : "10289747","mrds_id" : "W018008", "altername_or_previous_names": "Thompson Creek Tungsten Mine, Tungsten Jim Mine" }, "geometry": "POINT (-112.22194434499863 46.756337603970515)"},{"id":2,"Attributes": {"dep_id": "10022920", "mrds_id":"FS00436", "record_type":"Site"},"geometry": "POINT (-112.84298100894938 46.869337351353394)"}], "USMIN" : [ {"id":8, "Attributes": {"ftr_id":"Mf00576", "site_id":"ID00055", "ftr_name":"Tungsten Jim"}, "geometry":"POINT (-112.47445877319991 46.68033517492356)"}, {"id":10, "Attributes": {"ftr_id":"Mo00569", "site_id":"ID00055" }, "geometry":"POINT (-114.58608965399998 44.333859519000065)"} ]}')
+        description='Dictionary that stores the IDs point to other databases: e.g.: {{"source_ID":"MRDS", "record_ID":"10022920"}, {"source_ID":"USMIN", "record_id":"ID00055"}}')
 
 
 # Schemas can conform to other ones by inheriting from them or by declaring conformance
